4807e723f09af31f75e5acf15748e69e8c7dcd4c
minaungdain <ecotara@gmail.com>
2017-11-10 01:02:15 +0700

    authentication re-defined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { Config, IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular'
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
 import { AdminPage } from '../pages/admin/admin';
-import { Admin2Page } from '../pages/admin2/admin2';
-import { Admin3Page } from '../pages/admin3/admin3';
-import { Admin4Page } from '../pages/admin4/admin4';
-import { EmployeePage } from '../pages/employee/employee';
-import { Employee2Page } from '../pages/employee2/employee2';
-import { ManagerPage } from '../pages/manager/manager';
-import { Manager2Page } from '../pages/manager2/manager2';
-import { Manager3Page } from '../pages/manager3/manager3';
-import { Manager4Page } from '../pages/manager4/manager4';
-import { Manager5Page } from '../pages/manager5/manager5';
+//import { Admin2Page } from '../pages/admin2/admin2';
+//import { Admin3Page } from '../pages/admin3/admin3';
+//import { Admin4Page } from '../pages/admin4/admin4';
+//import { EmployeePage } from '../pages/employee/employee';
+//import { Employee2Page } from '../pages/employee2/employee2';
+//import { ManagerPage } from '../pages/manager/manager';
+//import { Manager2Page } from '../pages/manager2/manager2';
+//import { Manager3Page } from '../pages/manager3/manager3';
+//import { Manager4Page } from '../pages/manager4/manager4';
+//import { Manager5Page } from '../pages/manager5/manager5';
 //import { HomePage } from '../pages/home/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -31,22 +31,27 @@ var config = {
     storageBucket: "roomreservationsystem-7de09.appspot.com",
     messagingSenderId: "52821733340"
 };
+
+export const firebaseAuthConfig = {
+ // provider: AuthProviders.Password,
+ // method: AuthMethod.Password
+}
 firebase.initializeApp(Config);
 @NgModule({
   declarations: [
     MyApp,
     LoginPage,
     AdminPage,
-    Admin2Page,
-    Admin3Page,
-    Admin4Page,
-    EmployeePage,
-    Employee2Page,
-    ManagerPage,
-    Manager2Page,
-    Manager3Page,
-    Manager4Page,
-    Manager5Page,
+   // Admin2Page,
+   // Admin3Page,
+   // Admin4Page,
+  //  EmployeePage,
+  //  Employee2Page,
+  //  ManagerPage,
+  //  Manager2Page,
+   // Manager3Page,
+  //  Manager4Page,
+   // Manager5Page,
    // HomePage
   ],
   imports: [
@@ -60,22 +65,24 @@ firebase.initializeApp(Config);
     MyApp,
     LoginPage,
     AdminPage,
-    Admin2Page,
-    Admin3Page,
-    Admin4Page,
-    EmployeePage,
-    Employee2Page,
-    ManagerPage,
-    Manager2Page,
-    Manager3Page,
-    Manager4Page,
-    Manager5Page,
+  //  Admin2Page,
+  //  Admin3Page,
+  //  Admin4Page,
+  //  EmployeePage,
+  //  Employee2Page,
+  //  ManagerPage,
+  //  Manager2Page,
+  //  Manager3Page,
+  //  Manager4Page,
+  //  Manager5Page,
     //HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    StatusBar,
+    SplashScreen
   ]
 })
 export class AppModule {}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,107 +1,117 @@
+import { AuthProvider } from './../../providers/auth/auth';
 import { ToastController } from 'ionic-angular/umd';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AdminPage } from '../admin/admin';
-import { Admin2Page } from '../admin2/admin2';
+//import { Admin2Page } from '../admin2/admin2';
 //import { AdminPage } from '../admin/admin';
-import { Admin3Page } from '../admin3/admin3';
+//import { Admin3Page } from '../admin3/admin3';
 //import { AdminPage } from '../admin/admin';
-import { Admin4Page } from '../admin4/admin4';
+//import { Admin4Page } from '../admin4/admin4';
 //import { AdminPage } from '../admin/admin';
-import { EmployeePage } from '../employee/employee';
-import { Employee2Page } from '../employee2/employee2';
 //import { EmployeePage } from '../employee/employee';
-import { ManagerPage } from '../manager/manager';
-import { Manager2Page } from '../manager2/manager2';
+//import { Employee2Page } from '../employee2/employee2';
+//import { EmployeePage } from '../employee/employee';
 //import { ManagerPage } from '../manager/manager';
-import { Manager4Page } from '../manager4/manager4';
+//import { Manager2Page } from '../manager2/manager2';
 //import { ManagerPage } from '../manager/manager';
-import { Manager5Page } from '../manager5/manager5';
+//import { Manager4Page } from '../manager4/manager4';
 //import { ManagerPage } from '../manager/manager';
-import { Manager3Page } from '../manager3/manager3';
+//import { Manager5Page } from '../manager5/manager5';
+//import { ManagerPage } from '../manager/manager';
+//import { Manager3Page } from '../manager3/manager3';
 //import { ManagerPage } from '../manager/manager';
 //import { ManagerPage } from '../manager/manager';
 //import { SignupPage } from '../signup/signup';
-import { AuthProviders, AngularFireAuthProvider } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+@IonicPage({
+  name: 'login'
+})
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 
+//export interface User {
+//  email: string;
+//  password: string;
+//}
 export class LoginPage {
-  email: any;
-  password: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public AngularFire: AngularFireAuth){}
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth){}
 
 
   login() {
-    this.AngularFire.auth.login({email: this.email, password: this.password},
+   // this.afAuth.auth.signInWithEmailAndPassword({ }
 
     {
-      provider: AuthProviders.Password,
-      method: AuthMethods.Password
+     // provider: AuthProviders.Password,
+    //  method: AuthMethods.Password
     }
+  }
 }
 
   //signup() {
   //  this.navCtrl.push(SignupPage, { email: this.loginData.email});
 
  // }
-  goToAdmin(params){
-    if (!params) params = {};
-    this.navCtrl.push(AdminPage);
-  }goToAdmin2(params){
-    if (!params) params = {};
-    this.navCtrl.push(Admin2Page);
+ // goToAdmin(params){
+ //   if (!params) params = {};
+ //   this.navCtrl.push(AdminPage);
+ // }goToAdmin2(params){
+ //   if (!params) params = {};
+ //   this.navCtrl.push(Admin2Page);
   //}goToAdmin(params){
  //   if (!params) params = {};
  //   this.navCtrl.push(AdminPage);
-  }goToAdmin3(params){
-    if (!params) params = {};
-    this.navCtrl.push(Admin3Page);
+//  }goToAdmin3(params){
+ //   if (!params) params = {};
+//    this.navCtrl.push(Admin3Page);
  // }goToAdmin(params){
  //   if (!params) params = {};
   //  this.navCtrl.push(AdminPage);
-  }goToAdmin4(params){
-    if (!params) params = {};
-    this.navCtrl.push(Admin4Page);
+ // }goToAdmin4(params){
+  //  if (!params) params = {};
+  //  this.navCtrl.push(Admin4Page);
  // }goToAdmin(params){
  //   if (!params) params = {};
  //   this.navCtrl.push(AdminPage);
-  }goToEmployee(params){
-    if (!params) params = {};
-    this.navCtrl.push(EmployeePage);
-  }goToEmployee2(params){
-    if (!params) params = {};
-    this.navCtrl.push(Employee2Page);
+ // }goToEmployee(params){
+ //   if (!params) params = {};
+ //   this.navCtrl.push(EmployeePage);
+ // }goToEmployee2(params){
+ //   if (!params) params = {};
+ //   this.navCtrl.push(Employee2Page);
   //}goToEmployee(params){
   //  if (!params) params = {};
  //   this.navCtrl.push(EmployeePage);
-  }goToManager(params){
-    if (!params) params = {};
-    this.navCtrl.push(ManagerPage);
-  }goToManager2(params){
-    if (!params) params = {};
-    this.navCtrl.push(Manager2Page);
+//  }goToManager(params){
+ //   if (!params) params = {};
+ //   this.navCtrl.push(ManagerPage);
+//  }goToManager2(params){
+ //   if (!params) params = {};
+//    this.navCtrl.push(Manager2Page);
  // }goToManager(params){
  //   if (!params) params = {};
  //   this.navCtrl.push(ManagerPage);
-  }goToManager4(params){
-    if (!params) params = {};
-    this.navCtrl.push(Manager4Page);
+ // }goToManager4(params){
+  //  if (!params) params = {};
+ //   this.navCtrl.push(Manager4Page);
   //}goToManager(params){
  //   if (!params) params = {};
  //   this.navCtrl.push(ManagerPage);
-  }goToManager5(params){
-    if (!params) params = {};
-    this.navCtrl.push(Manager5Page);
+ // }goToManager5(params){
+ //   if (!params) params = {};
+ //   this.navCtrl.push(Manager5Page);
   //}goToManager(params){
   //  if (!params) params = {};
  //   this.navCtrl.push(ManagerPage);
-  }goToManager3(params){
-    if (!params) params = {};
-    this.navCtrl.push(Manager3Page);
+ // }goToManager3(params){
+ //   if (!params) params = {};
+  //  this.navCtrl.push(Manager3Page);
  // }goToManager(params){
  //   if (!params) params = {};
  //   this.navCtrl.push(ManagerPage);
